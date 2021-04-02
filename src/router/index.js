455306963f24ddb0e312555486cf406d6235f8fb
Kirtan Patel <kirtanpatl@gmail.com>
2021-04-02 12:48:40 -0400

    New personal site

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import TrackRUTerms from '../views/TrackRUTerms.vue';
+import TrackRUPrivacy from '../views/TrackRUPrivacy.vue';
+import DropletsPrivacy from '../views/DropletsPrivacy.vue';
 
 const routes = [
   {
@@ -7,13 +10,28 @@ const routes = [
     name: 'Home',
     component: Home,
   },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/trackruterms',
+    name: 'TrackRUTerms',
+    component: TrackRUTerms,
+  },
+  {
+    path: '/trackruPrivacyPolicy',
+    name: 'TrackRUPrivacy',
+    component: TrackRUPrivacy,
+  },
+  {
+    path: '/Dropelet.Privacy.Policy',
+    name: 'DropletsPrivacy',
+    component: DropletsPrivacy,
   },
 ];
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,101 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1>Kirtan Patel</h1>
+      <p>Coming soon</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;900&display=swap');
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+}
+
+p {
+  font-family: 'Roboto', sans-serif;
+  color: #43505e52;
+  margin-top: 0;
+}
+
+h1 {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  vertical-align: middle;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 900;
+  font-size: 100px;
+  color: transparent;
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 0.67),
+    rgba(255, 255, 255, 0.25)
+  ),
+  linear-gradient(180deg, #feac5e, #c779d0, #4bc0c8);
+  background-clip: text;
+  margin-bottom: 0;
+}
+
+.home {
+  background: linear-gradient(
+    0deg,
+    rgba(255, 255, 255, 0.671),
+    rgba(255, 255, 255, 0.67)
+  ),
+  linear-gradient(270deg, #feac5e, #c779d0, #4bc0c8);
+  background-size: 600% 600%;
+
+  -webkit-animation: gradientMove 30s ease infinite;
+  -moz-animation: gradientMove 30s ease infinite;
+  animation: gradientMove 30s ease infinite;
+  position: absolute; top: 0; left: 0; height: 100%; width: 100%;
+}
+
+@-webkit-keyframes gradientMove {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+@-moz-keyframes gradientMove {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+@keyframes gradientMove {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+</style>
