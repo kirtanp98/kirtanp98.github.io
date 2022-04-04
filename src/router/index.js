2ca71556c1063f00a745c04c8fad52894efb82e6
Kirtan Patel <kirtanpatl@gmail.com>
2022-04-04 16:45:01 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -10,29 +10,6 @@ const routes = [
     name: 'Home',
     component: Home,
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  // },
-  {
-    path: '/trackruterms',
-    name: 'TrackRUTerms',
-    component: TrackRUTerms,
-  },
-  {
-    path: '/trackruPrivacyPolicy',
-    name: 'TrackRUPrivacy',
-    component: TrackRUPrivacy,
-  },
-  {
-    path: '/Dropelet.Privacy.Policy',
-    name: 'DropletsPrivacy',
-    component: DropletsPrivacy,
-  },
 ];
 
 const router = createRouter({
